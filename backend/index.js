471d982f3fcaf2a49a191ee13b7fc4ef23b8c093
muhammedadilkk912 <adilkkmuhammed85@gmail.com>
2025-08-07 10:51:41 +0530

    change cors origin

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,8 @@ dotenv.config()
 const app=express()
 console.log("secret key=",process.env.STRIPE_SECRET_KEY)
 app.use(cors({  
-    origin: 'http://localhost:5173',
+    // origin: 'http://localhost:5173',
+    origin:'https://mrig-mart-iuk2.vercel.app/',
     credentials: true // ✅ Important for cookies/sessions
 }));
 app.use(passport.initialize())
